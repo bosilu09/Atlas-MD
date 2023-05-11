3b2f5efaf8fc8f827b347cdc0538672ea241c3c2
bosilu09 <84651759+bosilu09@users.noreply.github.com>
2023-05-12 02:15:20 +0530

    Updated script message 🐱‍👤

--- Commands/Core/script.js
+++ Commands/Core/script.js
@@ -9,9 +9,9 @@ module.exports = {
     category: "Core",
     start: async(Miku, m,{pushName,prefix}) => {
         let picURL = fs.readFileSync('./Page/BG.jpg')
-        let repoInfo = await axios.get('https://api.github.com/repos/FantoX001/Atlas-MD')
+        let repoInfo = await axios.get('https://api.github.com/repos/bosilu09/nova')
         let repo = repoInfo.data
-        let txt = `      🧣 *${botName}'s Script* 🧣\n\n*🎀 Total Forks:* ${repo.forks_count}\n*⭐ Total Stars:* ${repo.stargazers_count}\n*📜 License:* ${repo.license.name}\n*📁 Repo Size:* ${(repo.size/1024).toFixed(2)} MB\n*📅 Last Updated:* ${repo.updated_at}\n\n*🔗 Repo Link:* ${repo.html_url}\n\n❝ Dont forget to give a Star ⭐ to the repo. It's made with restless harkwork by *Team ATLAS*. ❞\n\n*©️ Team ATLAS- 2023*`
-        await Miku.sendMessage(m.from,{image:picURL, caption:txt},{quoted:m});
+        let txt = `      🧣 *${botName}'s Script* 🧣\n\n*🐱‍👤 Total Forks:* ${repo.forks_count}\n*⭐ Total Stars:* ${repo.stargazers_count}\n*📜 License:* ${repo.license.name}\n*📁 Repo Size:* ${(repo.size/1024).toFixed(2)} MB\n*📅 Last Updated:* ${repo.updated_at}\n\n*🔗 Repo Link:* ${repo.html_url}\n\n❝ Dont forget to give a Star ⭐ to the repo. It's made with restless harkwork by *Team NOVA*. ❞\n\n*_©️ Team NOVA- 2023*_`
+        await Miku.sendMessage(m.from,{image:{url:"https://graph.org/file/27054bdb82718a29de80c.png"}, caption:txt},{quoted:m});
     }
 }
