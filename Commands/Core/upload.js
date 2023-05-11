2b812d0b047edd405ca6c245e6395a626026c6f0
bosilu09 <84651759+bosilu09@users.noreply.github.com>
2023-05-12 04:24:13 +0530

    Update upload.js

--- Commands/Core/upload.js
+++ Commands/Core/upload.js
@@ -1,1 +1,10 @@
-
+module.exports = {
+    name: "upload",
+    alias: ["hello","info"],
+    desc: "Say hello to bot.",
+    react: "⚡",
+    category: "Core",
+    start: async(Miku, m,{pushName,prefix}) => {
+      Miku.sendMessage
+    }
+}
